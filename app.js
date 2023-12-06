ab415477cebd78e91d4c60ef4a04330644062f66
Taz Campbell <shelbyacampbell2724@gmail.com>
2023-12-06 13:19:58 -0500

    not sure why the results aren't displaying. Going to work on CSS

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
+// const url = "https://www.dnd5eapi.co/api/monsters"
+// fetch(url)
+// .then((res) => res.json())
+// .then((data) => console.log(data))
+
 const url = "https://www.dnd5eapi.co/api/monsters"
-fetch(url)
-.then((res) => res.json())
-.then((data) => console.log(data))
+let challengeRating = document.getElementById('challengeRating')
+const button = document.getElementById('search')
+const resultsList = document.getElementById('results')
+
+
+button.addEventListener('click', () => {
+    const selectedChallengeRating = challengeRating.value;
+  
+    let urlWithChallengeRating = url;
+    if (selectedChallengeRating !== " ") {
+      urlWithChallengeRating += `?challenge_rating=${selectedChallengeRating}`;
+    }
+  
+    fetch(urlWithChallengeRating)
+      .then(response => response.json())
+      .then(data => {
+        resultsList.innerHTML = "";
+        data.results.forEach(monster => {
+          const listItem = document.createElement("li");
+          listItem.textContent = monster.name;
+          resultsList.appendChild(listItem);
+        });
+      })
+  });
